6515830c9f794db49586c20710eb706aa47ecd2c
Yangyang Pan <yyangpan@gmail.com>
2016-08-29 11:57:44 -0700

    perform pci rescan without opening /sysfs

--- pci_rescan.c
+++ pci_rescan.c
@@ -5,7 +5,7 @@
 #include <linux/fs.h>
 #include <asm/segment.h>
 #include <asm/uaccess.h>
-//#include <linux
+#include <linux/pci.h>
 
 /*
  * kthread_create: will create a new task_struct with no process address space
@@ -25,56 +25,26 @@ typedef struct pci_rescan {
 
 #define PCI_SCAN_FILE	"/sys/bus/pci/rescan"
 
-pci_rescan_t scan_info;	
+pci_rescan_t scan_info;
 
-struct file *
-pci_open(const char *path, int flags, int mode) 
+static void
+do_pci_scan(void)
 {
-	struct file *filp;
-	mm_segment_t oldfs;
-	oldfs = get_fs();
-	set_fs(get_ds());
-	filp = filp_open(path, flags, mode);
-	set_fs(oldfs);
-	return filp;
-}
+  struct pci_bus *b = NULL;
+  pci_lock_rescan_remove();
 
-int
-pci_write(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size)
-{
-	mm_segment_t	old_fs;
-	int ret;
-	loff_t	pos = offset;
-	old_fs = get_fs();
-	set_fs(get_ds());
-	spin_lock(&file->f_lock);
-	file->f_pos = pos;
-	spin_unlock(&file->f_lock);
-	ret = vfs_write(file, data, size, &pos);
-	// or ret = file->f_op->write(file, data, size, &pos);
-	set_fs(old_fs);
-	return (ret);
+  while ((b = pci_find_next_bus(b)) != NULL) {
+    pci_rescan_bus(b);
+  }
+  pci_unlock_rescan_remove();
 }
 
 int
 perform_pci_rescan(void *data)
 {
-	char val[] = "1";
-	//scan_info.pcifd = pci_open(PCI_SCAN_FILE, O_WRONLY, S_IRWXU|S_IRWXG|S_IRWXO);
-	scan_info.pcifd = pci_open(PCI_SCAN_FILE, O_WRONLY, 0777);
-	if (IS_ERR(scan_info.pcifd)) {
-		printk("failed to open %s %ld\n", PCI_SCAN_FILE, PTR_ERR(scan_info.pcifd));
-		scan_info.task = NULL;
-		do_exit(0);
-		return (-1);
-	}
 	while (!kthread_should_stop()) {
 		ssleep(5);
-    /*
-     * Need to write a string value to perform pci-rescan.
-     * bus_rescan_store() does kstrtoul().
-     */
-		pci_write(scan_info.pcifd, 0, val, strlen(val));
+		do_pci_scan();
 	}
 	return (0);
 }
@@ -91,7 +61,7 @@ pci_init_module(void)
 	if (IS_ERR(scan_info.task)) {
 		err = PTR_ERR(scan_info.task);
 		scan_info.task = NULL;
-		return err; 
+		return err;
 	}
 	wake_up_process(scan_info.task);
 	return (err);
@@ -100,9 +70,6 @@ pci_init_module(void)
 void
 pci_exit_module(void)
 {
-	if (!IS_ERR(scan_info.pcifd)) {
-		filp_close(scan_info.pcifd, NULL);
-	}
 	//if (!IS_ERR(scan_info.task)) {
 	if (scan_info.task) {
 		kthread_stop(scan_info.task);
